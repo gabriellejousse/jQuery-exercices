cfc8f6c4efec20d7ed5007d1667f255b9bc77c2f
gabriellejousse <gabrielle.jousse@gmail.com>
2020-09-16 13:29:27 +0200

    sauvegarde 16/09

--- djquery_adventure_starter/exercice29/js/main.js
+++ djquery_adventure_starter/exercice29/js/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 
     $('.btn').click(function(){
+        // on stocke la valeur de l'input dans une variable (le texte qui sera écrit dedans):
+        var inputText = $('input').val()
 
         // si la valeur (le texte dans l'input) est supérieure à 5 caractères, ajouter la class 'is-valid' 
-        if ($('input').val().length > 5){
+        if (inputText.length >= 5){
+            // on doit lui demander avant de supprimer la classe is-invalid si elle est la (sinon il y a les deux classes qui s'accumulent)
+            $('input').removeClass('is-invalid')
+            // puis on ajoute la class 'is-valid' :
             $('input').addClass('is-valid')
+            
         } else {
+            // on doit lui demander avant de supprimer la classe is-valid si elle est la (sinon il y a les deux classes qui s'accumulent)
+            $('input').removeClass('is-valid')
+            // puis on ajoute la class 'is-invalid' :
             $('input').addClass('is-invalid')
 
         }
         
     })
 })
-
-// OK
--- djquery_adventure_starter/exercice30/js/main.js
+++ djquery_adventure_starter/exercice30/js/main.js
@@ -2,10 +2,13 @@ $(document).ready(function () {
 
     $('input').keyUp(function(){
 
+        var inputText = $('input').val();
         
-        if ($('input').val().length > 5){
+        if (inputText.length > 5){
+            $('input').removeClass('is-invalid')
             $('input').addClass('is-valid')
         } else {
+            $('input').removeClass('is-valid')
             $('input').addClass('is-invalid')
         }
     })
